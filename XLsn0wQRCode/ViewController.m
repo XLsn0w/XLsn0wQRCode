9cd6d55e5a3c20fbbaa2c8227946c67f802ca9a8
xlsn0w <xlsn0w@outlook.com>
2018-01-09 12:41:43 +0800

    init

---
+++ XLsn0wQRCode/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  XLsn0wQRCode
+//
+//  Created by ginlong on 2018/1/9.
+//  Copyright © 2018年 ginlong. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
